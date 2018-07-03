b1c1e22323c19c24d083e77e3543fdeb6ede2372
Sajid <mmsajid@gmail.com>
2018-07-03 16:50:47 +0000

    removed sudo su and -y to ppa

--- ansible-intallation-script.sh
+++ ansible-intallation-script.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-sudo su
+
 sudo apt-get -y update
 sudo apt-get -y install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get -y update
 sudo apt-get -y install ansible
